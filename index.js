f810e5385d85014c9a41f9e22d25787406e2af7b
Kane Baccigalupi <baccigalupi@gmail.com>
2020-09-13 09:13:35 -0700

    Replaces axios proxy with http module

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const StaticServer = require('./lib/StaticServer')
 const fileMapper = require('./lib/fileMapper')
-const Proxy = require('./lib/Proxy')
+const Proxy = require('./lib/HttpProxy')
 
 Proxy.StaticServer = StaticServer
 Proxy.fileMapper = fileMapper
